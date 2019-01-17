332b315b5b56533375391b314587af0d321ff4f4
Eduardo - Notebook <eduardo-tell@hotmail.com>
2019-01-16 23:52:47 -0200

    Update NegociacaoController.js

--- client/js/app/controller/NegociacaoController.js
+++ client/js/app/controller/NegociacaoController.js
@@ -1,7 +1,25 @@
 class NegociacaoController {
+
+    // Definindo que esta classe sempre terá estes dados para reutilização
+    constructor(){
+        let $ = document.querySelector.bind(document);
+        this._inputData = $("#data");
+        this._inputQuantidade = $("#quantidade");
+        this._inputValor = $("#valor");
+    }
     
     adiciona(event){
         event.preventDefault();
-        alert('Chamou controller!');
+
+        // Criando uma negociação com os dados respectivos
+        let negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negociacao);
+
+        // Adicionar a negociação em uma lista
     }
 }
